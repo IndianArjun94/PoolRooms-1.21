2ad7165acdff51df2242b45e8fcb7072ef5be98b
angam <arjun459459459@gmail.com>
2025-07-12 16:56:35 -0500

    Initial Commit

---
+++ src/main/java/net/arjun/poolrooms/PoolRoomsClient.java
@@ -0,0 +1,10 @@
+package net.arjun.poolrooms;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PoolRoomsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
