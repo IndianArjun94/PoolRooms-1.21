02363eb83d7130181eba72ab83c83c558c302ef2
angam <arjun459459459@gmail.com>
2025-07-19 18:01:19 -0500

    Trial 2 skybox

--- src/main/java/net/arjun/poolrooms/block/custom/LightSkyboxBlock/LightSkyboxBlockEntity.java
+++ src/main/java/net/arjun/poolrooms/block/custom/LightSkyboxBlock/LightSkyboxBlockEntity.java
@@ -1,11 +1,12 @@
 package net.arjun.poolrooms.block.custom.LightSkyboxBlock;
 
+import net.arjun.poolrooms.block.ModBlocks;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.util.math.BlockPos;
 
 public class LightSkyboxBlockEntity extends BlockEntity {
     public LightSkyboxBlockEntity(BlockPos pos, BlockState state) {
-        super(YourMod.SKYBOX_BLOCK_ENTITY_TYPE, pos, state);
+        super(ModBlocks.SKYBOX_BLOCK_ENTITY_TYPE, pos, state);
     }
 }
